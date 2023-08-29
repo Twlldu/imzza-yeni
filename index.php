358c8627487ef7c7cb59b6e528d3e38b788361ae
utku odabasi <utkuodabasi44@gmail.com>
2023-08-29 18:43:27 +0300

    first commit

---
+++ index.php
@@ -0,0 +1,122 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="./src/css/style.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@10/swiper-bundle.min.css"/>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css" integrity="sha512-z3gLpd7yknf1YoNbCzqRKc4qyor8gaKU1qmn+CShxbuBusANI9QpRohGBreCFkKxLhei6S9CQXFEbbKuqLg0DA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+</head>
+<body>
+
+    <!-- navbar started-->
+
+    <header class="nav-container">
+
+            <div class="nav-menu container">
+                <div class="logo">
+                    <a href="#"><img src="./src/img/imzza-orange.png" alt="" srcset=""></a>
+                </div>
+                <ul class="menu-list">
+                    <li class="list-item"><a href="#">Anasayfa</a></li>
+                    <li class="list-item"><a href="#">Hakkımızda</a></li>
+                    <li class="list-item"><a href="#">İletişim</a></li>
+                    <li class="list-item"><a href="#">Referanslar</a></li>
+                    <li class="list-item"><a href="#">Hizmetlerimiz</a></li>
+                </ul>
+                <div class="nav-login">
+                    <div class="login-menu">
+                        <ul class="login-list">
+                            <li class="login-item"><a href="#">Giriş</a></li>
+                            <li class="login-item"><a href="#">Kayıt</a></li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+
+    </header>
+
+    <!-- navbar end-->
+
+    <!-- Herosection end-->
+
+    <section class="heroSection-container">
+        <img class="heroSection-background" src="./src/img/herosection.jpg" alt="" srcset="">
+            <div class="heroMenu container">
+                <div class="heroCard">
+                    <div class="heroCard-header">
+                        <h1 class="head">Ne Hizmet Almak İsterdiniz ?</h1>
+                    </div>
+                    <div class="heroCard-main">
+                        <form action="" method="get">
+                            <input class="searchBox" type="text" name="searchname" Placeholder="Ne Aramak İstemiştiniz ?" id=""> <button name="sbt" class="btn btn-primary" type="submit"></button>
+                        </form>
+                    </div>
+                    <div class="heroCard-bottom">
+
+                    </div>
+                </div>
+            </div>
+    </section>
+
+    <!-- Herosection end-->
+
+    <!-- Services started -->
+
+    <section class="services-container">
+        <div class="services-menu container">
+            <div class="services-header">
+                <div class="servicesHeader-grid">
+                    <div class="grid-1">
+                        <form action="" method="get">
+                            <button class="grid-btn " t ype="submit">Web Tasarım</button>
+                        </form>
+                    </div>
+                    <div class="grid-2">
+                        <form action="" method="get">
+                            <button class="grid-btn " type="submit">Sosyal Medya</button>
+                        </form>
+                    </div>
+                    <div class="grid-3">
+                        <form action="" method="get">
+                            <button class="grid-btn " type="submit">Patent</button>
+                        </form>
+                    </div>
+                    <div class="grid-4">
+                        <form action="" method="get">
+                            <button class="grid-btn " type="submit">Kurumsal Kimlik</button>
+                        </form>
+                    </div>
+                    <div class="grid-5">
+                        <form action="" method="get">
+                            <button class="grid-btn " type="submit">Grafik Tasarım</button>
+                        </form>
+                    </div>
+                </div>
+            </div>
+            <div class="services-main">
+                <div class="services-card-grid">
+                    <div class="services-card">
+                        <div class="services-card-header">
+
+                        </div>
+                        <div class="services-card-main">
+
+                        </div>
+                        <div class="services-card-bottom">
+
+                        </div>
+                    </div>
+                </div>        
+            </div>
+            <div class="services-bottom">
+
+            </div>
+        </div>
+    </section>
+
+    <!-- Services end -->
+
+</body>
+</html>
